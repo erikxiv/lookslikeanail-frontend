18a350fcf094432c1e75f36387adc30b09ed7555
Erik Larsson <erik.larsson@musiker.nu>
2015-07-29 13:56:28 +0200

    Reverting some changes to see if that helps travis test not to crash

--- Brocfile.js
+++ Brocfile.js
@@ -19,7 +19,7 @@ var app = new EmberApp({
   }
 });
 
-app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+// app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
