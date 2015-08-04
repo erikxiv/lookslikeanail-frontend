25176a400f9afb48b4dbe00bb6c4453ae3741feb
Erik Larsson <erik.larsson@musiker.nu>
2015-08-04 18:08:52 +0200

    Minor styling work to get menu working on mobile

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  menuShown: true,
+  menuShown: Ember.$(window).width() >= 1024,
   toolFilterEnabled: function() {
     return this.currentRouteName === 'tools';
   }.property('currentRouteName'),
   actions: {
     toggleMenu: function() {
       this.set('menuShown', ! this.get('menuShown'));
+      // Make button lose focus (or it will still be highlighted by bootstrap css)
+      Ember.$('.navbar-toggle').blur();
     }
   }
 });
