82a676715231cec86b5d54176f7007100169ca90
Erik Larsson <erik.larsson@musiker.nu>
2015-08-19 00:11:47 +0200

    Download readme

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -17,7 +17,8 @@ export default Ember.Controller.extend({
             description: item.description,
             created: item.created_at,
             updated: item.updated_at,
-            icon: item.owner.avatar_url
+            icon: item.owner.avatar_url,
+            url: item.url
           });
         }));
         that.set('githubstatus', 'Done');
@@ -30,6 +31,15 @@ export default Ember.Controller.extend({
       var wasExpanded = item.get('isExpanded');
       this.github.setEach('isExpanded', false);
       item.set('isExpanded', ! wasExpanded);
+      if (! wasExpanded) {
+        // Get readme
+        var that = this;
+        Ember.$.getJSON(item.url+'/readme').then(
+          function(response) {
+            item.set('description', atob(response.content));
+          }
+        );
+      }
     }
   }
 });
